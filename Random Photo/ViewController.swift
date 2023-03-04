33114466f47472674aad4cd2a9e3d04a38f3b8e4
ssimondev <simi.simecekseznam.cz>
2023-03-04 15:18:06 +0100

    Initial Commit

---
+++ Random Photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Photo
+//
+//  Created by Simon on 3/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
