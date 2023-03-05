428f7ce4e4f71ae76e8c6213041ebf132de22f2d
ssimondev <simi.simecekseznam.cz>
2023-03-05 16:03:21 +0100

    Added random photo on restart

--- Random Photo/ViewController.swift
+++ Random Photo/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemCyan
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 250, height: 250)
+        imageView.center = view.center
+        view.addSubview(button)
+        getRandomPhoto()
     }
-
-
+    
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data =  try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
 }
 
